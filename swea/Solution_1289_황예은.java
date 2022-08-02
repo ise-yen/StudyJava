6c40ee8625848f05bc6ca1c33b343ddb466f48b0
YeEun <66158433+ise-yen@users.noreply.github.com>
2022-08-02 10:21:17 +0900

    Add no.1289

---
+++ swea/Solution_1289_황예은.java
@@ -0,0 +1,28 @@
+package swea;
+
+import java.util.Scanner;
+
+// 원재의 메모리 복구하기
+public class Solution_1289_황예은 {
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++) {
+			int res = 0;
+			String str = sc.next();
+		
+			for(int i = 0; i< str.length(); i++) {
+				if(i == 0) {
+					if(str.charAt(i) == '1') res++;
+				}
+				else {
+					if(str.charAt(i) != str.charAt(i-1)) res++;
+				}
+			}
+
+			System.out.println("#" + test_case + " " + res);
+		}
+	}
+
+}
