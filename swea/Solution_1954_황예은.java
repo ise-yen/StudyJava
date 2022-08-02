08b2bff4d2c119b39bf1bc868cc8c03771444660
YeEun <66158433+ise-yen@users.noreply.github.com>
2022-08-02 18:03:15 +0900

    Add 1954 file

---
+++ swea/Solution_1954_황예은.java
@@ -0,0 +1,47 @@
+package swea;
+
+// 달팽이 숫자
+import java.util.Scanner;
+
+public class Solution_1954_황예은 {
+
+	public static void main(String[] args) throws Exception {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int test_case = 1; test_case < T; test_case++) {
+			int n = sc.nextInt();
+			int map[][] = new int[n][n];
+			int snail = 1;
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					if (map[i][j] == 0) {
+						map[i][j] = snail++;
+					}
+				}
+				for (int j = n - 1; j >= 0; j--) {
+					if (i == n - 1)
+						map[i][j] = snail++;
+				}
+			}
+			for (int i = 0; i < n; i++) {
+			}
+			for (int i = 0; i < n; i++) {
+				for (int j = n - 1; j >= 0; j--) {
+					if (i == n - 1 && map[i][j] == 0)
+						map[i][j] = snail++;
+				}
+			}
+			for (int i = n - 1; i >= 0; i--) {
+				for (int j = n - 1; j >= 0; j--) {
+					if (j == 0 && map[i][j] == 0)
+						map[i][j] = snail++;
+				}
+			}
+
+			System.out.println("#" + test_case);
+			System.out.println();
+		}
+	}
+
+}
