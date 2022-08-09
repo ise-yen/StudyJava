ce8e36b58a4a52656cfcc202d1b011616cd4adef
YeEun <66158433+ise-yen@users.noreply.github.com>
2022-08-09 09:04:32 +0900

    Update and rename Main_2023_황예은.java to Main_2023_신기한소수.java

--- BackUp/Main_2023_신기한소수.java
+++ BackUp/Main_2023_신기한소수.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-
-public class Main_2023_황예은 {
+// 신기한소수
+public class Main_2023_신기한소수 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
