40ae838f52d7da2de736b6d1b664d6fdff10c4d4
YeEun <66158433+ise-yen@users.noreply.github.com>
2022-08-09 09:18:29 +0900

    Update and rename Solution_2001_황예은.java to Solution_2001_파리퇴치.java

--- swea/Solution_2001_파리퇴치.java
+++ swea/Solution_2001_파리퇴치.java
@@ -1,6 +1,6 @@
 
 import java.util.Scanner;
-public class Solution_2001_황예은 {
+public class Solution_2001_파리퇴치 {
 	public static void main(String args[]) throws Exception
 	{
 		Scanner sc = new Scanner(System.in);
