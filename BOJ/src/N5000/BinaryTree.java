c35db1ac2789dc9e431d25c58ee2300b39ff599e
yeEun <dkffls11@naver.com>
2022-08-09 18:01:12 +0900

    스터디 문제 풀다 말았음

---
+++ BOJ/src/N5000/BinaryTree.java
@@ -0,0 +1,13 @@
+package N5000;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTree {
+	Node head;
+	int size;
+	
+	public BinaryTree() {
+		head = null;
+		size= 0;
+	}
+}
