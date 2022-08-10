668fdbc1c86c381fa7a32654bf4e9064aa02e031
yeEun <dkffls11@naver.com>
2022-08-10 18:01:06 +0900

    [BOJ] 1009_분산처리 실패

--- BOJ/src/N1000/Main_1009_분산처리.java
+++ BOJ/src/N1000/Main_1009_분산처리.java
@@ -1,9 +1,22 @@
 package N1000;
 
+import java.util.Scanner;
+
 public class Main_1009_분산처리 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		for(int t = 0; t < T; t++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int data = (int)Math.pow(a, b);
+			while(data < 10) {
+				data %= 10;
+			}
+			System.out.println(data);
+		}
 
 	}
 
