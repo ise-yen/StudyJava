cca6e6a2548e942e36ba6fc573f40a4e360e7aa6
yeEun <dkffls11@naver.com>
2022-08-10 17:52:57 +0900

    오류 주석처리

--- BOJ/src/N5000/BinaryTree.java
+++ BOJ/src/N5000/BinaryTree.java
@@ -1,13 +1,13 @@
-package N5000;
-import java.util.LinkedList;
-import java.util.Queue;
-
-public class BinaryTree {
-	Node head;
-	int size;
-	
-	public BinaryTree() {
-		head = null;
-		size= 0;
-	}
-}
+//package N5000;
+//import java.util.LinkedList;
+//import java.util.Queue;
+//
+//public class BinaryTree {
+//	Node head;
+//	int size;
+//	
+//	public BinaryTree() {
+//		head = null;
+//		size= 0;
+//	}
+//}
