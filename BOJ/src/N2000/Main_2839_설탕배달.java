7dda7d8d05d180fef78febb364795ab2c3d5a751
YeEun <ydkrkek@gmail.com>
2022-08-16 13:14:37 +0900

    [Upload] boj_2839_설탕배달

---
+++ BOJ/src/N2000/Main_2839_설탕배달.java
@@ -0,0 +1,28 @@
+package N2000;
+
+import java.util.Scanner;
+
+public class Main_2839_설탕배달 {
+	static int N;
+	// 5kg, 3kg
+	static int kg5 = 5;
+	static int kg3 = 3;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		System.out.println(Suger());
+	}
+
+	static int Suger() {
+		for (int cnt5 = N / kg5; cnt5 >= 0; cnt5--) {
+			for (int cnt3 = 0; cnt3 <= N / kg3; cnt3++) {
+				if (N == kg5 * cnt5 + kg3 * cnt3) {
+					return cnt5 + cnt3;
+				}
+			}
+		}
+		return -1;
+	}
+
+}
