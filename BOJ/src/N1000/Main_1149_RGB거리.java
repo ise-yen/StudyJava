061e60987251f70a963d9c8f8f005807c453aae9
YeEun <ydkrkek@gmail.com>
2022-10-02 16:07:42 +0900

    [Upload] boj_1149_rgb거리 : 복습필요

--- BOJ/src/N1000/Main_1149_RGB거리.java
+++ BOJ/src/N1000/Main_1149_RGB거리.java
@@ -11,21 +11,58 @@ public class Main_1149_RGB거리 {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());// 집 번호 : 1~N번
-		int[][] cost = new int[N][3]; // N개의 집, rgb
-		int[][] minCostColor = new int[N][3];
-		for (int n = 1; n <= N; n++) {
+		int[][] cost = new int[N + 1][3]; // N개의 집, rgb
+		int[][] minCostColor = new int[N + 1][3]; // N개 집의 제일 적은 비용의 색상
+		for (int n = 1; n < cost.length; n++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int minCost = Integer.MAX_VALUE; // i층에서 제일 작은 비용 찾기...를 1~N까지
-			int minColor = 0;
 			for (int c = 0; c < 3; c++) {
 				cost[n][c] = Integer.parseInt(st.nextToken());
-				if (minCost > cost[n][c]) {
-					minColor = c;
-				}
 			}
 		}
 		// ======== 입력 끝
 
+		for (int n = 1; n < cost.length; n++) {
+			minCostColor[n][0] = Math.min(minCostColor[n - 1][1], minCostColor[n - 1][2]) + cost[n][0];
+			minCostColor[n][1] = Math.min(minCostColor[n - 1][0], minCostColor[n - 1][2]) + cost[n][1];
+			minCostColor[n][2] = Math.min(minCostColor[n - 1][1], minCostColor[n - 1][0]) + cost[n][2];
+		}
+		res = Math.min(Math.min(minCostColor[N][0], minCostColor[N][1]), minCostColor[N][2]);
+
+//		int start = 0;
+//		for (int n = 1; n < N; n++) {
+//			if (minCostColor[n] == minCostColor[n + 1]) {
+//				// 다음 집과 다른 색상으로
+//				int c1 = 0, c2 = 0;
+//				switch (minCostColor[n]) {
+//				case 0:
+//					c1 = 1;
+//					c2 = 2;
+//					break;
+//				case 1:
+//					c1 = 0;
+//					c2 = 2;
+//					break;
+//				case 2:
+//					c1 = 0;
+//					c2 = 1;
+//					break;
+//				}
+//				if (cost[n][c1] < cost[n][c2]) {
+//					start += cost[n][c1];
+//				} else if (cost[n][c1] > cost[n][c2]) {
+//					start += cost[n][c2];
+//				} else {
+//					if (c1 == minCostColor[n])
+//						start += cost[n][c2];
+//					else
+//						start += cost[n][c1];
+//				}
+//			} else {
+//				start += cost[n][minCostColor[n]];
+//			}
+//		} // 집 다 돌았음
+//		res = start;
+		System.out.println(res);
 	}
 }
 
