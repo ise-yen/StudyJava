03533cd85a32ce34a5e35f8774d43e5e99eae463
황예은 <dkffls11@naver.com>
2022-10-07 17:17:54 +0900

    [Update] swea_4014_활주로 : 실패

---
+++ BOJ/src/N10000/Main_12869_뮤탈리스크.java
@@ -0,0 +1,34 @@
+package N10000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_12869_뮤탈리스크 {
+	static int[] scvHP;
+
+	public static void main(String[] args) throws IOException {
+		int mutalCnt = 1;
+		int res = 0; // 정답 : 모든 SCV를 파괴하기 위해 공격해야 하는 횟수의 최솟값
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int scvCnt = N;
+		scvHP = new int[N]; // scv 체력 ( 0 < hp <= 60)
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			scvHP[i] = Integer.parseInt(st.nextToken());
+		}
+		// =============== 입력 종료
+
+	}
+
+	static void attack(int idx, int cnt) {
+		// 한 번에 3개의 scv 공격 가능
+		// 1 공격 : -9
+		// 2 공격 : -3
+		// 3 공격 : -1
+	}
+}
---
+++ SWEA/src/InterestCalculator.java
@@ -0,0 +1,39 @@
+
+public class InterestCalculator {
+
+	static double rate = 0.11; // 금리
+	static int inputMoney = 300000; // 납입 금액
+	static int period = 6; // 납입 기간
+
+	public static void main(String[] args) {
+		double resultMoney = 0; // 최종 금액
+		double resultInterest = 0; // 최종 이자
+
+		// 기간동안 적금 납입 성공 시
+		for (int p = period; p >= 1; p--) {
+			double inter = calInterest(p, period, inputMoney);
+			resultMoney += inputMoney + inter;
+			resultInterest += inter;
+		}
+
+		// 소수점 자르고 결과값 출력
+		System.out.println("최종금액 : " + Math.round(resultMoney));
+		System.out.println("최종이자 : " + Math.round(resultInterest));
+	}
+
+	// 주차별 이자 계산하는 함수
+	static double calInterest(int p, int period, int inputMoney) { // 예치회차, 계약회차, 납입 금액
+		double interest = 0; // 이자
+
+		// 이자계산방법 (세전) : 회차별 입금금액 X 약정금리 X 예치회차 / 계약회차
+		interest = inputMoney * rate * ((double) p / (double) period);
+
+		interest = Math.round(interest); // 소수점 날림
+
+		// 이자 출력
+		System.out.println(Math.abs(period - p) + 1 + "회차 이자 : " + interest);
+
+		return interest;
+	}
+
+}
