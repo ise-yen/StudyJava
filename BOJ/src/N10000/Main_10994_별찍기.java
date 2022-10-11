b27066c1e55981bb0d0d1b1e60a074f2c0cb80fd
YeEun <ydkrkek@gmail.com>
2022-10-11 13:12:11 +0900

    [Upload] boj_10994_별찍기

---
+++ BOJ/src/N10000/Main_10994_별찍기.java
@@ -0,0 +1,42 @@
+package N10000;
+import java.util.Scanner;
+
+// 별찍기
+public class Main_10994_별찍기 {
+//	위쪽
+	static void stars(int n) {
+		for (int i = 0; i < (n - 1) * 4; i++)
+			System.out.print("*");
+		System.out.println("*");
+	}
+
+	static void starsLeft() {
+		System.out.print("*");
+		for(int i =0; i< )
+	}
+	static void blank() {
+		System.out.print(" ");
+	}
+	static void starsRightJump() {
+
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		stars(n);
+		
+
+	}
+
+}
+//*********
+//*       *
+//* ***** *
+//* *   * *
+//* * * * *
+//* *   * *1 3 1
+//* ***** * 1 1
+//*       *  7
+//*********
