68e88d6f4fdf25325826572b4ed2a44068571fb6
YeEun <ydkrkek@gmail.com>
2022-10-11 12:56:04 +0900

    [Update] boj_1244_스위치켜고끄 이름 수정

--- BOJ/src/N1000/Main_1244_스위치켜고끄기.java
+++ BOJ/src/N1000/Main_1244_스위치켜고끄기.java
@@ -2,7 +2,7 @@ package N1000;
 
 import java.util.Scanner;
 
-public class Main_1244_스위치켜고크기 {
+public class Main_1244_스위치켜고끄기 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
