ea33da2cb2c57d7cebe5e2d2d7c41964222f8fc7
YeEun <ydkrkek@gmail.com>
2022-10-29 18:48:57 +0900

    백업

---
+++ BOJ/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static class load{
+		
+	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int N  = Integer.parseInt(st.nextToken());
+		int D = Integer.parseInt(st.nextToken());
+	}
+}
